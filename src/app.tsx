8cabf589148dd85f58a3640e18b111f8813e5087
ViewAR ViewAR <viewar@it5.at>
2019-11-24 21:44:03 +0100

    Added scss configuration

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { hot } from 'react-hot-loader/root'
+import styles from './app.module'
+import './global'
 
 function App() {
-  return <h2>This is our Reactivengst application!</h2>
+  return <h2 className={styles.red}>This is our Reactivengst application!</h2>
 }
 
 export default hot(App)
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,76 @@
-const isDevelopment = process.env.NODE_ENV === 'development'
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const isDevelopment = process.env.NODE_ENV === "development";
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: isDevelopment ? 'development' : 'production',
-    module: {
-      rules: [
-        {
-          test: /\.(t|j)sx?$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/
-        },
-        {
-            test: /\.html$/,
-            use: [
-              {
-                loader: 'html-loader',
-                options: { minimize: !isDevelopment }
-              }
-            ]
-          }
-      ]
-    },
-    resolve: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"]
-    },
-    output: {
-        filename: isDevelopment ? '[name].js' : '[name].[hash].js'
+  mode: isDevelopment ? "development" : "production",
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
       },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebPackPlugin({
-            template: './src/index.html',
-            filename: './index.html'
-          })
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: !isDevelopment }
+          }
+        ]
+      },
+      {
+        test: /\.module\.s(a|c)ss$/,
+        loader: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: isDevelopment
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        loader: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      }
     ]
-  }
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".scss"]
+  },
+  output: {
+    filename: isDevelopment ? "[name].js" : "[name].[hash].js"
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? "[name].css" : "[name].[hash].css",
+      chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].css"
+    })
+  ]
+};
