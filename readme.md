ad97645dacc94851ef175725c1d92974916be5b5
ViewAR ViewAR <viewar@it5.at>
2019-12-29 20:15:15 +0100

    Added routing of sample pages

--- readme.md
+++ readme.md
@@ -2,3 +2,11 @@ Used this tutorial:
 https://developerhandbook.com/webpack/webpack-4-from-absolute-scratch/
 
 https://developerhandbook.com/webpack/getting-started-with-webpack-dev-server/
+
+Es6 in depth
+https://hacks.mozilla.org/category/es6-in-depth/
+
+react router
+https://reacttraining.com/react-router/web/guides/quick-start
+
+TODO: https://learnwithparam.com/blog/creating-404-page-with-react-router/
--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,114 @@
 import React from 'react'
 import { hot } from 'react-hot-loader/root'
-import styles from './css/app.module'
+// import styles from './css/app.module'
+import { Link, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './global'
 
-function App() {
-  return <h2 className={styles.red}>This is our Reactivengst application!</h2>
+const styles = require('./css/app.module');
+
+
+// https://learnwithparam.com/blog/dynamic-pages-in-react-router/
+const users = [
+  {
+    name: `Param`,
+  },
+  {
+    name: `Vennila`,
+  },
+  {
+    name: `Afrin`,
+  },
+];
+
+// react routing example
+const IndexPage = () => {
+  return (
+    <h3>Home Page</h3>
+  );
+};
+
+const AboutPage = () => {
+  return (
+    <h3>About Page</h3>
+  );
+};
+
+// userId will be Array index + 1
+const UsersPage = () => {
+  return (
+    <>
+      {users.map((user, index) => (
+        <h5 key={index}>
+          <Link to={`/user/${index + 1}`}>{user.name}'s Page</Link>
+        </h5>
+      ))}
+    </>
+  );
+};
+
+const UserPage = ({ match, location }) => {
+  const { params: { userId } } = match;
+  return (
+    <>
+      <p>
+        <strong>User ID: </strong>
+        {userId}
+      </p>
+      <p>
+        <strong>User Name: </strong>
+        {users[userId - 1].name}
+      </p>
+      <p>
+        <strong>Match Props: </strong>
+        <code>{JSON.stringify(match)}</code>
+        {/* { "path": "/user/:userId", "url": "/user/1", "isExact": true, "params": { "userId": "1" } } */}
+
+      </p>
+      <p>
+        <strong>Location Props: </strong>
+        <code>{JSON.stringify(location, null, 2)}</code>
+      </p>
+    </>
+  );
+};
+
+const SearchPage = ({ match, location }) => {
+  return (
+    <p>
+      <strong>Location Props: </strong>
+      <div>{location.search}</div>
+      {/* {JSON.stringify(location, null, 2)} */}
+    </p>
+  );
 }
+
+const NoMatchPage = () => {
+  return (
+    <h3>404 - Not found</h3>
+  );
+};
+
+const  App = () => {
+  return ( 
+    <section className="App">
+      <Router>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/users">Users</Link>
+        <Link to="/search?q=react">Search</Link>
+        <Link to="/404-not-found">404</Link>
+        <Switch>
+          <Route exact path="/" component={IndexPage} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path='/users' component={UsersPage} />
+          <Route exact path='/user/:userId' component={UserPage} />
+          <Route exact path="/search" component={SearchPage} />
+          <Route component={NoMatchPage} />
+        </Switch>
+      </Router>
+      <a href="/about">about with browser reload</a>
+    </section>
+  );
+};
 
 export default hot(App)
